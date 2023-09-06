3e4ca1554af475be82bfae002502ea484e7c2996
WolverinLando <orlandourbanotrejo@gmail.com>
2023-09-05 23:12:22 -0600

    Algoritmo que calcula la multiplicacion de dos numeros

---
+++ Ejercicio11.php
@@ -0,0 +1,14 @@
+<?php
+/* Autor:  Orlando Urbano Trejo @Lando
+ * Fecha:  04-09-2023
+ * Correo: orlandourbanotrejo@gmail.com
+ * Algoritmo que calcula la multiplicacion de dos numeros */
+
+// Entrada de datos
+echo "Ingresa un numero: ";
+$numero1 = readline();
+echo "Ingresa un otro numero: ";
+$numero2 = readline();
+$solucion = $numero1 * $numero2;
+echo "Resultado: ".$solucion."\n";
+?>
